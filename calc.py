7563f82e844d6415a4732276c979acc946d083a0
pazhukov <k.a.pazukov@gmail.com>
2023-11-11 02:19:54 +0300

    add base operators

--- calc.py
+++ calc.py
@@ -1,2 +1,9 @@
 class SimpleCalc:
     res = 0.0
+    operators = {
+        '+': lambda res, x: res + x,
+        '-': lambda res, x: res - x,
+        '*': lambda res, x: res * x,
+        '/': lambda res, x: res / x,
+        '%': lambda res, x: res % x,
+     }
