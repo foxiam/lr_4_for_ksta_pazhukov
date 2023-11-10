5fd3f638ef9af209f86b1acda6a9aefcfcb525de
pazhukov <k.a.pazukov@gmail.com>
2023-11-11 02:25:40 +0300

    add function for input args and final of endpoint

--- calc.py
+++ calc.py
@@ -6,7 +6,13 @@ class SimpleCalc:
         '*': lambda res, x: res * x,
         '/': lambda res, x: res / x,
         '%': lambda res, x: res % x,
-     }
+    }
+
+    def input(self):
+        inp = input(f'{self.res} ')
+        [operator, number] = (inp.strip().split() + ['0'])[:2]
+        number = float(number) if '.' in number else int(number)
+        self.calc(operator, number)
     
     def calc(self, operator: str, number: int | float):
         try:
--- main.py
+++ main.py
@@ -2,3 +2,5 @@ from calc import SimpleCalc
     
 if __name__ == "__main__":
     calulator = SimpleCalc()
+    while True:
+        calulator.input()
